686ef96f6e0841495d3784648f0f7f803401e662
khairnsaa <k.nsaa95@gmail.com>
2023-05-18 10:34:27 +0700

    create simple api route and fetch in FE

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,29 @@
 import express from 'express'
+import products from './data/products.js'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
+
+// Enable CORS middleware
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send('HELLLOOO')
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+    res.json(products.find(product => product._id === req.params.id))
 })
 
 app.listen(port, () => console.log(`listening to port: ${port}`))
--- frontend/src/screens/HomeScreens.jsx
+++ frontend/src/screens/HomeScreens.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Col, Row } from 'react-bootstrap'
-import products from '../products'
+// import products from '../products'
 import Product from '../components/Product'
+import axios from 'axios'
 
 const HomeScreens = () => {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/products').then(result => setProducts(result.data))
+  })
+  
   return (
-    <>
-        <h1>Latest Products</h1>   
-        <Row>
-            {
-                products.map(product => (
-                    <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-                        <Product product={product} />
-                    </Col>
-                ))
-            }
-        </Row>
-    </>
+  <>
+    <h1>Latest Products</h1>   
+    <Row>
+      {
+        products.map(product => (
+          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+            <Product product={product} />
+          </Col>
+        ))
+      }
+    </Row>
+  </>
   )
 }
 
